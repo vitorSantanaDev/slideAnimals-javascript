cbfa5069619d6fc10065a4962c62da4f11938d20
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-16 12:00:20 -0300

    ✨added methods to calculate distances in events

--- src/js/controller/controllerSlide.js
+++ src/js/controller/controllerSlide.js
@@ -2,16 +2,32 @@ export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide);
+    this.distance = { finalPosition: 0, startX: 0, move: 0 };
   }
 
   onStart(event) {
     event.preventDefault();
+    this.distance.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
-  onMove(event) {}
+  updatePosition(clienX) {
+    this.distance.move = (this.distance.startX - clienX) * 1.6;
+    return this.distance.finalPosition - this.distance.move;
+  }
+
+  moveSlide(distanceX) {
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
+  }
+
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onEnd(event) {
+    this.distance.finalPosition = this.distance.movePosition;
     this.wrapper.removeEventListener("mousemove", this.onMove);
   }
 
