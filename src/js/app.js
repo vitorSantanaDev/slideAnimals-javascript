dba888547cbac49ba036f96d5862306cde9d2f1a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-22 11:23:39 -0300

    ✨ Introduce buttons next and prev

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-import Slide from "./controller/controllerSlide.js";
+import { SlideNav } from "./controller/controllerSlide.js";
 
-const slide = new Slide('.slideWrapper', '.slideItems');
-slide.init()
+const slide = new SlideNav(".slideWrapper", ".slideItems");
+slide.init();
+slide.addArrow(".prev", ".next");
 
-slide.changeSlide(3)
