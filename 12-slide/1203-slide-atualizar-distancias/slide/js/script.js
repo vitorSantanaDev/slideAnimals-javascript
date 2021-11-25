c844794842e309b99cb18ddcdb12b28a427c79fd
Vitor Santana <86623759+vitorSantanaDev@users.noreply.github.com>
2021-11-25 11:24:45 -0300

    Delete 12-slide directory

--- 12-slide/1203-slide-atualizar-distancias/slide/js/script.js
+++
@@ -1,5 +0,0 @@
-import Slide from './slide.js';
-
-const slide = new Slide('.slide', '.slide-wrapper');
-slide.init();
-console.log(slide);
--- 12-slide/1210-slide-custom-controls/slide/js/script.js
+++
@@ -1,6 +0,0 @@
-import SlideNav from './slide.js';
-
-const slide = new SlideNav('.slide', '.slide-wrapper');
-slide.init();
-slide.addArrow('.prev', '.next');
-slide.addControl('.custom-controls');
